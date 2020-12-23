a9e05a159e1d82af685201d616583ed010ccbb1c
Hoang <hoang.nguyen2@ntq-solution.com.vn>
2020-12-23 10:15:39 +0700

    Updated: default port

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
 require("./app/routes/verb.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
